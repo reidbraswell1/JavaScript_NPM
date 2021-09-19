103255f6693d58cbc6c2e7f99cadb5d933e058b0
Reid Braswell <reidbras@gmail.com>
2021-09-19 15:27:44 -0500

    Initial Revision

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("---Begin start.js---");
+let dayjs = require('dayjs');
+dayjs('2018-08-08'); // parse
+
+let myDate = dayjs().format('YYYY-MM-DDTHH:mm:ss A'); // display
+console.log(`The Current Date is ${myDate}`);
+
+var http = require('http');
+var fs = require('fs');
+function onRequest(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html', null, function(error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+}
+http.createServer(onRequest).listen(5000);
