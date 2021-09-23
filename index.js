3393ee5f934e2fd50bfa84342943dabd349ca8ce
reid <reidbras@gmail.com>
2021-09-22 20:54:21 -0500

    Adding express and ejs

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 console.log("---Begin start.js---");
-let dayjs = require('dayjs');
+const express = require('express');
+const dayjs = require('dayjs');
+
+// Express
+const app = express();
+
 dayjs('2018-08-08'); // parse
 
 let myDate = dayjs().format('YYYY-MM-DDTHH:mm:ss A'); // display
 console.log(`The Current Date is ${myDate}`);
 
+/*
 let http = require('http');
 let fs = require('fs');
 function onRequest(request, response) {
@@ -20,3 +26,21 @@ function onRequest(request, response) {
     });
 }
 http.createServer(onRequest).listen(5000);
+*/
+// View engine setup
+app.set('view engine', 'ejs');
+app.use("/styles",express.static(__dirname + "/styles"));
+app.get("/", (rew,res) => {
+    try {
+        res.status(200);
+        res.render('index', {
+            title: "NPM Node JS Exercise",
+            heading: "NPM Node JS Exersise",
+        });
+    }
+    catch(error) {
+        res.status(500);
+        res.send(`A server error occurred while handling your request error = ${error}`);
+    }
+});
+app.listen(3000,() => console.log("Server listening on port 3000"));
